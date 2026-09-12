00e2910ce02c5cde502294294b0dbbdd0daa8473
Bramtheking <bramwela8@gmail.com>
2026-09-12 20:26:12 +0300

    first commit

--- assets/useNavigate-B0OATmgd.js
+++
@@ -1,1 +0,0 @@
-import{i as e,o as t}from"./createLucideIcon-Qk0VrUvi.js";import{r as n}from"./link-CWAOiTup.js";var r=t(e(),1);function i(e){let t=n();return r.useCallback(n=>t.navigate({...n,from:n.from??e?.from}),[e?.from,t])}export{i as t};
